5783baf951e9abe5b80c1fedf5915970a26d4cb1
dinhlbv2026-coder <dinhlbv2026@gmail.com>
2026-09-24 21:58:17 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='be-tap-may-tinh-v20';
+const CACHE='be-tap-may-tinh-v24';
 const CORE=['./','index.html','emoji.json','manifest.webmanifest','icon-192.png','icon-512.png','icon-maskable.png','apple-touch-icon.png'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
